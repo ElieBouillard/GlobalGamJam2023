41f7b8435a07b815b89b50b1586b01529642238f
Robin Six <six.robin@hotmail.fr>
2023-02-03 21:07:39 +0100

    Movement and camera base code.

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody _rigidbody = null;
+    [SerializeField] private Camera _camera = null;
+
+    [Header("Movement")]
+    [SerializeField, Min(0f)] private float _movementSpeed = 10f;
+    
+    [Header("Camera")]
+    [SerializeField, Min(0f)] private float _cameraRotationSpeed = 180f;
+
+    [Header("Dash")]
+    [SerializeField, Min(0f)] private float _dashDuration = 0.5f;
+    [SerializeField, Min(0f)] private float _dashDistance = 5f;
+
+    private Vector2 _movementInput;
+    private Vector2 _mouseInput;
+    private bool _attackInput;
+    private bool _dashInput;
+
+    private void RegisterInputs()
+    {
+        _movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        _attackInput = Input.GetMouseButtonDown(0);
+        _dashInput = Input.GetKeyDown(KeyCode.LeftShift);
+    }
+
+    private void CleanupInputs()
+    {
+        _movementInput = Vector2.zero;
+        _attackInput = false;
+        _dashInput = false;
+    }
+
+    private void RotateCamera()
+    {
+        float speed = _cameraRotationSpeed / 0.016f;
+        Vector3 angularVelocity = new Vector3(_mouseInput.x * speed, _mouseInput.y * speed);
+        angularVelocity *= Time.deltaTime;
+
+        Vector3 eulerAngles = _camera.transform.localEulerAngles;
+        eulerAngles.y += angularVelocity.x;
+        eulerAngles.x -= angularVelocity.y;
+        _camera.transform.localEulerAngles = eulerAngles;
+    }
+
+    private void Move()
+    {
+        Vector3 velocity = new Vector3(_movementInput.x, 0f, _movementInput.y);
+        velocity = Quaternion.Euler(0f, _camera.transform.localEulerAngles.y, 0f) * velocity;
+        velocity.Normalize();
+        velocity *= _movementSpeed;
+        _rigidbody.velocity = velocity;
+    }
+
+    private void Update()
+    {
+        RegisterInputs();
+    }
+
+    private void LateUpdate()
+    {
+        RotateCamera();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+}
