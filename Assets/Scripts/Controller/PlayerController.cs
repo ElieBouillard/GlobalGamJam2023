b136c3c46789d7706bfdbddb105e103835834379
Robin Six <six.robin@hotmail.fr>
2023-02-03 22:06:11 +0100

    Camera pitch.

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour
     
     [Header("Camera")]
     [SerializeField] private Vector2 _cameraRotationSpeed = new Vector2(45f, 45f);
+    [SerializeField] private Vector2 _cameraPitchLimit = new Vector2(-60f, 60f);
 
     [Header("Dash")]
     [SerializeField, Min(0f)] private float _dashDuration = 0.5f;
@@ -47,6 +48,12 @@ public class PlayerController : MonoBehaviour
         Vector3 eulerAngles = _camera.transform.localEulerAngles;
         eulerAngles.y += angularVelocity.x;
         eulerAngles.x -= angularVelocity.y;
+
+        bool lookingUp = _camera.transform.forward.y > 0f;
+        eulerAngles.x = lookingUp
+                        ? Mathf.Max(eulerAngles.x - 360f, _cameraPitchLimit.x) + 360f
+                        : Mathf.Min(eulerAngles.x, _cameraPitchLimit.y);
+
         _camera.transform.localEulerAngles = eulerAngles;
     }
 
