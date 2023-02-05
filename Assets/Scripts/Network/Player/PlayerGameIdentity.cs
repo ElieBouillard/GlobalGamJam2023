c42e0a877911ad8bdf859fa2b7f2a0b08e76980a
Elie <75082918+ElieBouillard@users.noreply.github.com>
2023-02-05 16:37:56 +0100

    [ADD] - Player Name

--- Assets/Scripts/Network/Player/PlayerGameIdentity.cs
+++ Assets/Scripts/Network/Player/PlayerGameIdentity.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Steamworks;
+using TMPro;
 using UnityEngine;
 
 public class PlayerGameIdentity : PlayerIdentity
@@ -9,10 +10,18 @@ public class PlayerGameIdentity : PlayerIdentity
     public MovementReceiver MovementReceiver { private set; get; }
 
     public RemoteWeaponSelection RemoteSelection { private set; get; }
+
+    [SerializeField] private TMP_Text _nameText;
+    
     
     private void Awake()
     {
         MovementReceiver = GetComponent<MovementReceiver>();
         RemoteSelection = GetComponent<RemoteWeaponSelection>();
+    }
+
+    public void SetName(string name)
+    {
+        _nameText.text = name;
     }
 }
